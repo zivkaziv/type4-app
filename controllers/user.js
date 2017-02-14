9b093227f26b8601b1b935f51dd74eca75df1ffe
zivkaziv <zivkalderon95@gmail.com>
2017-02-14 19:22:30 +0200

    Update the allergies in server api

--- controllers/user.js
+++ controllers/user.js
@@ -144,6 +144,7 @@ exports.accountPut = function(req, res, next) {
       user.gender = req.body.gender;
       user.location = req.body.location;
       user.website = req.body.website;
+      user.allergies = req.body.allergies;
     }
     user.save(function(err) {
       if ('password' in req.body) {
