953a9abd103845d96c5a568e1f1c7b4ce5848837
zivkaziv <zivkalderon95@gmail.com>
2017-07-05 15:05:34 +0300

    phase 1 -allergies detection

--- controllers/allergies.js
+++ controllers/allergies.js
@@ -1,9 +1,9 @@
-
-var allergiesBuilder = require('../business/logic/allergiescollectionbuilder');
-var Allergy = require('../models/Allergy');
 /**
  * POST /buildallergies
  */
+
+var allergiesBuilder = require('../business/logic/allergiescollectionbuilder');
+var Allergy = require('../models/Allergy');
 
 var allergiesCache = [];
 exports.buildAllergiesCollectionPost = function(req, res) {
@@ -23,12 +23,62 @@ exports.getAllAllergiesGet = function(req, res) {
 
 exports.analyzeAllergiesPost = function(req,res){
     console.log(req.body);
+    var fullProducts = [];
+    var allergiesDetected = [];
     var products = req.body.products;
-    if(allergiesCache.length === 0){
-        Allergy.find({}, function(err, allergies) {
-            allergiesCache = allergies;
+
+    handleAllergies().then(()=>{
+        products.map((product) =>{
+            fullProducts.push(findProduct(product))
         });
-    }
-    // { _id : { $in : [1,2,3,4] } }
-    res.send([]);
+
+        fullProducts.map((product) =>{
+            allergiesDetected.push(getAllergyFromProduct(allergiesCache,product))
+        });
+
+        res.send(allergiesDetected);
+    });
+
 };
+
+function findProduct(product){
+    return product;
+}
+
+function getAllergyFromProduct(allergies,product){
+    var ingredient_analysis =[];
+    if(allergies && product){
+        for (let ingredientIndex = 0; ingredientIndex < product.ingredients.length; ingredientIndex++) {
+            let isSensitiveIngredient = false;
+            for (let allergyIndex = 0; allergyIndex < allergies.length; allergyIndex++) {
+                if (allergies[allergyIndex].compound.toLowerCase().indexOf(product.ingredients[ingredientIndex].toLowerCase()) > -1) {
+                    ingredient_analysis.push({
+                        name: product.ingredients[ingredientIndex],
+                        analysis: 'SENSITIVE'
+                    });
+                    isSensitiveIngredient = true
+                }
+            }
+            if(!isSensitiveIngredient){
+                ingredient_analysis.push({
+                    name: product.ingredients[ingredientIndex],
+                    analysis: 'NOT_SENSITIVE'
+                })
+            }
+        }
+    }
+    return ingredient_analysis;
+}
+
+function handleAllergies(){
+    return new Promise((resolve,reject) => {
+        if (allergiesCache.length === 0) {
+            Allergy.find({}, function (err, allergies) {
+                allergiesCache = allergies;
+                resolve(allergiesCache);
+            });
+        } else {
+            resolve(allergiesCache);
+        }
+    });
+}
